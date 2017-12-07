835186eac9d2ffab1f12850a07c4eaee43aac81a
TeoBlock <31297100+TeoBlock@users.noreply.github.com>
2017-12-07 06:55:41 -0500

    Add files via upload

---
+++ M2HW1_DistanceTravelled_McIntireTheodore.py
@@ -0,0 +1,39 @@
+# CTI-110
+# M2HW1 - Distance Traveled 
+# Theodore McIntire
+# 14 September 2017
+#This program calculates the distrance travelled for different times
+
+#Declare variables and set their values to zero
+distance6hours = 0
+
+distance10hours = 0
+
+distance15hours = 0
+
+#Declare CONSTANTS and identify their values
+SPEED = 70
+
+SIX_HOURS = 6
+
+TEN_HOURS = 10
+
+FIFTEEN_HOURS = 15
+
+#variables are calculated to equal SPEED multiplied by HOURS
+
+distance6hours = SPEED * SIX_HOURS
+
+distance10hours = SPEED * TEN_HOURS
+
+distance15hours = SPEED * FIFTEEN_HOURS
+
+print ("The vehicle is going 70 MPH and will travel the following distances")
+
+print ("The distance traveled in 6 hours is", distance6hours)
+
+print ("The distance traveled in 10 hours is", distance10hours)
+
+print ("The distance traveled in 15 hours is", distance15hours)
+
+#end of program
